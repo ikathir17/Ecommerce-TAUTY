50235f85231d5ddd7b2ffa7f7bcde347f0891202
Kathiresan <kathiresanp80152@gmail.com>
2025-06-14 08:15:33 +0530

    Cover image

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -238,7 +238,7 @@ const Home = () => {
                 sx={{
                     position: 'relative',
                     height: isMobile ? '70vh' : '90vh',
-                    backgroundImage: 'url(https://images.unsplash.com/photo-1485462537746-965f33f7f6a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80)',
+                    backgroundImage: 'url(/images/cover.jpg)',
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                     display: 'flex',
